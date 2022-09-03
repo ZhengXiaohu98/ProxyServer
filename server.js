f1625f4c23c8cc6ceb1e47ca4e20380dd4bb6b79
Xiaohu Zheng <zxh635148320@gmail.com>
2022-09-02 22:49:27 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const cors = require("cors")
+app.use(cors())
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+app.use('/api.getlcinfo', createProxyMiddleware({
+    // target url
+    target: 'https://leetcode.com/graphql',
+    changeOrigin: true,
+}));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
+console.log('Proxy server is listen at port ' + PORT);
